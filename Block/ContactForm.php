5a6690efa47f39afd08cd004e24a0daf3da6e27f
Yaroslav Kozar <kozar.yaroslav1@gmail.com>
2021-03-30 14:58:20 +0300

    Task5ChangeProductPrice

---
+++ Block/ContactForm.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace Kozar\HelloWorld\Block;
+use Magento\Framework\View\Element\Template;
+
+class ContactForm extends \Magento\Contact\Block\ContactForm{
+
+    public function getText()
+    {
+        return "Override Text";
+    }
+}
